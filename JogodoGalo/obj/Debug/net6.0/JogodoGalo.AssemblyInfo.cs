5b064e89040363630808d0533b8634be59c5d08b
Raúl Arieiro <raularieiro20@gmail.com>
2024-02-28 12:02:27 +0000

    O commit

--- JogodoGalo/obj/Debug/net6.0/JogodoGalo.AssemblyInfo.cs
+++ JogodoGalo/obj/Debug/net6.0/JogodoGalo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogodoGalo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa959bfb0ca4f6f3d76aec54c5bde0fa90d2c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b610c1c764492a5395f8c2137c911be4c9e2b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogodoGalo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogodoGalo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
